7f6b07b7a87587584f423907eb29465ed919f63a
A <aastaotnes@gmail.com>
2019-01-27 09:39:53 +1300

    Ben - event boi ded :(

--- TeamTeamHomeAlone/Assets/TeamTeamHomeAlone/Scripts/Lure.cs
+++ TeamTeamHomeAlone/Assets/TeamTeamHomeAlone/Scripts/Lure.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Lure : MonoBehaviour
 {
+    public bool On = false;
     public float Durability = 100f;
+    public int Uses = 3;
     // Update is called once per frame
     void Update()
     {
         if (Durability < 0f)
         {
             //Debug.Log("Lure Broke");
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            On = false;
+        }
+    }
+
+    void OnTriggerStay(Collider _col)
+    {
+        if (_col.gameObject.tag == "Item" && !On && Uses > 0)
+        {
+            Debug.Log("Lure on");
+            Uses--;
+            On = true;
+            Durability = 100f;
+            Destroy(_col.gameObject);
         }
     }
 }
